fd92648752a6c9dd270235f6ad5563c92302c9bd
DusanChromy <Dusan.Chromy@deutschebahn.com>
2025-11-24 21:01:38 +0100

    feat: add log output
    Task:

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-export PATH=$PATH:~/dotfiles/bin
+export PATH=$PATH:$HOME/dotfiles/bin
 
+echo Install.sh is running >> ~/.dotfiles.log
+echo PATH is now $PATH >> ~/.dotfiles.log
 echo Custom setup done! Git commands are now available.
