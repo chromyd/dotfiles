e2b2010aaba4c27dd8c94dd327812402acceaec0
DusanChromy <Dusan.Chromy@deutschebahn.com>
2025-11-24 21:53:03 +0100

    feat: update setup
    Task:

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-echo install.sh is running >> ~/.dotfiles.log
-
-exec 3> ~/.dotfiles.log
-BASH_XTRACEFD=3
+exec > ~/.dotfiles.log 2>&1  
 set -x
 
 mkdir ~/.local
 ln -s $HOME/dotfiles/bin ~/.local
 
-#echo "export PATH=$PATH:$HOME/dotfiles/bin
 cat ~/dotfiles/.bashrc >> ~/.bashrc
 
 echo Custom setup done! Git commands are now available.
