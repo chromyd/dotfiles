b3a1373b1d665a92765d9623a2e1e64f2e09f1c6
DusanChromy <Dusan.Chromy@deutschebahn.com>
2025-11-25 13:23:29 +0100

    feat: make switching between p10k and other themes easy
    Task:

--- install.sh
+++ install.sh
@@ -3,14 +3,21 @@
 exec > ~/.dotfiles.log 2>&1  
 set -x
 
+P10K=true
+
 mkdir ~/.local
 ln -s $HOME/dotfiles/bin ~/.local
-
-cat ~/dotfiles/.bashrc >> ~/.bashrc
-cat ~/dotfiles/.zshrc >> ~/.zshrc
-
 ln -s ~/dotfiles/{.p10k.zsh,.zsh_aliases,.zsh_functions} ~
 
-# Uncomment the next line to change ZSH_THEME
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-sed -i 's/^ZSH_THEME="[^"]*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+cat ~/dotfiles/.bashrc >> ~/.bashrc
+
+if ${P10K}
+then
+    cat ~/dotfiles/.zshrc-p10k >> ~/.zshrc
+    sed -i 's/^ZSH_THEME="[^"]*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+else
+    cat ~/dotfiles/.zshrc >> ~/.zshrc
+    # Uncomment the next line to change ZSH_THEME
+    # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+    # sed -i 's/^ZSH_THEME="[^"]*"/ZSH_THEME="af-magic"/' ~/.zshrc
+fi
